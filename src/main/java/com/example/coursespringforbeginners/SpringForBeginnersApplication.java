06fbd6c9df2ec5923c229758da99137392264f22
Mavluda Raximquliyeva <m.raximquliyeva@uzinfocom.uz>
2024-06-07 10:43:48 +0500

    first commit

---
+++ src/main/java/com/example/coursespringforbeginners/SpringForBeginnersApplication.java
@@ -0,0 +1,13 @@
+package com.example.coursespringforbeginners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForBeginnersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringForBeginnersApplication.class, args);
+    }
+
+}
