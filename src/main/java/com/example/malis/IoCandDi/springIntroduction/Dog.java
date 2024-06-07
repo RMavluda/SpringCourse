63314f9dcaebd82cd8d0f4b57ab73e6f1e370add
Mavluda Raximquliyeva <m.raximquliyeva@uzinfocom.uz>
2024-06-07 15:03:22 +0500

    new files

---
+++ src/main/java/com/example/malis/IoCandDi/springIntroduction/Dog.java
@@ -0,0 +1,7 @@
+package com.example.malis.IoCandDi.springIntroduction;
+
+public class Dog {
+    public void Say(){
+        System.out.println("Bow-Wow");
+    }
+}
---
+++ src/main/java/com/example/malis/IoCandDi/springIntroduction/Test1.java
@@ -0,0 +1,8 @@
+package com.example.malis.IoCandDi.springIntroduction;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.Say();
+    }
+}
